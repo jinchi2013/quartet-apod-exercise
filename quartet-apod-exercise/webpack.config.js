f391b978144ac29672e264cbf4e8d24f2c33e5e0
Chi Jin <jinchi2013us@gmail.com>
2017-05-06 00:54:53 -0400

    add gitignore and some comment for webpack

--- quartet-apod-exercise/webpack.config.js
+++ quartet-apod-exercise/webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var loaders = [
+  // this the loader config to load js files
   {
     "test": /\.js?$/,
     "exclude": /node_modules/,
@@ -19,21 +20,32 @@ var loaders = [
 ];
 
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: path.resolve('src', 'main.js'),
+  devtool: 'eval-source-map', // source map for debug
+  entry: path.resolve('src', 'main.js'), // entry point of the app
   output: {
-    path: path.resolve('build'),
-    filename: '[name].js',
+    path: path.resolve('build'), // output file folder
+    filename: '[name].js', // output filename
     publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve('src', 'index.tpl.html'),
       inject: 'body',
-      filename: 'index.html'
+      filename: 'index.html' // The file to write the HTML to. Defaults to index.html.
     })
   ],
   module: {
     loaders: loaders
   }
 };
+
+/*
+HtmlWebpackPlugin({
+  template: // the template used for html,
+  inject: //true | 'head' | 'body' | false Inject all assets into the given template or templateContent -
+          When passing true or 'body' all javascript resources will be placed at the bottom of the body element.
+          'head' will place the scripts in the head element.
+  filename: // The file to write the HTML to. Defaults to index.html.
+})
+
+*/
